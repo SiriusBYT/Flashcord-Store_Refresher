7eb45c85c5c6794545af1abfd03e77bad21b54ba
Sirius B <root.siriusb@gmail.com>
2024-01-23 21:21:00 +0100

    good

--- Flashcord-Store_Refresher.py
+++ Flashcord-Store_Refresher.py
@@ -1,6 +1,9 @@
 from flashapi import *
 import time
 import random
+import math
+
+StartTime = time.time()
 
 API_Version = "2.01"
 Copyright_Date = "2024"
@@ -14,6 +17,29 @@ StoreTemplate = "store/templates/store_template.html"
 # Don't touch this, it will get overwritten anyways but still. Don't touch just in case.
 HTMLFile = ""
 
+SplashText = [
+    "Oh look, a splash text!",
+    "Now built thanks to the Flashstore API!",
+    "Still without Javascript!",
+    "The Discount Replugged Store™️",
+    "Without working search bars!",
+    "now with more yaes than mikos! \n - Atacantul, 2024 (from GangChat)"
+]
+SplashText_Length = len(SplashText) - 1
+SplashSeed = random.randint(0,SplashText_Length)
+SplashText_Selected = SplashText[SplashSeed]
+
+
+
+print(SplashText_Selected)
+
+def GetAPILatency():
+    InitTime = time.time()
+    FlashClient_API_Request("GET")
+    Latency = math.floor((time.time() - InitTime) * 1000)
+    Latency_String = str(Latency) + "ms"
+    return Latency_String
+API_Latency = GetAPILatency()
 
 def GetCode(GetWhat):
     HTMLCode = ''
@@ -48,69 +74,57 @@ def GetCode(GetWhat):
             Results = CallAPI("UserThemes",str(Users[cycle]))
         if Results != None:
             Results = Results.replace("[","").replace("]","").replace('"','').replace(" ","").split(",")
-            for cycle in range (len(API_Folders)):
-                API_Folders[cycle] = API_Folders[cycle] + "-files"
-                HTMLCode = HTMLCode + '<iframe class="Flashcord-Module_Embed" src="store/modules/' + Users[cycle] + Results[cycle] + '/embed.html"></iframe>\n'
-
-    #HTMLCode = "<h3>It's bloody empty in here!\n"
+            for subcycle in range (len(Results)):
+                TotalX.append(Users[cycle] + "/" + Results[subcycle] + "-files")
+    TotalX.sort()
+    if GetWhat == "Modules":
+        EmbedType = "modules"
+    elif GetWhat == "Plugins":
+        EmbedType = "plugins"
+    elif GetWhat == "Themes":
+        EmbedType = "themes"
+    if TotalX != []:
+        for cycle in range (len(TotalX)):
+            HTMLCode = HTMLCode + '<iframe class="Flashcord-Module_Embed" src="store/' + EmbedType + '/' + TotalX[cycle] + '/embed.html"></iframe>\n'
+    else:
+        HTMLCode = "<h3>It's bloody empty in here!</h3>\n"
     return HTMLCode
 
 ModuleCode = GetCode("Modules")
-print(ModuleCode)
+PluginCode = GetCode("Plugins")
+ThemesCode = GetCode("Themes")
+#print(ModuleCode)
+#print(PluginCode)
+#print(ThemesCode)
 
-"""
-def HTMLConfigurator():
-    print('[FlashCFG // HTML-CFG] Connecting to SGN servers...')
-    MoreByCode = GetEmbedCode()
-    print(f'[FlashCFG // HTML-CFG] The "More by" section will have:\n{MoreByCode}')
-    
-
+def HTMLConfigurator(ShowBuildTime):
+    HTMLFile = "store.html"
     with open(StoreTemplate, 'r', encoding='utf-8') as StoreTemplate_File:
-        with open(EmbedTemplate, 'r', encoding='utf-8') as EmbedTemplate_File:
+        if ShowBuildTime == False:
             with open(HTMLFile, 'w', encoding='utf-8') as EditHTML_File:
                 EditHTML_File.write("")
             with open(HTMLFile, 'a', encoding='utf-8') as EditHTML_File:
-                if Step == 0:
-                    HTMLArray = StoreTemplate_File.readlines()
-                elif Step == 1:
-                    HTMLArray = EmbedTemplate_File.readlines()
-                else:
-                    print("[FlashCFG // HTML-CFG] WARNING: Sirius A was here and caused another Big Bang", '(What the fuck is Step "', Step, '"?!)')
-                    print("[FlashCFG // HTML-CFG] Also how in the LIVING FUCK DID YOU TRIGGER THIS ERROR without triggering the previous one?")
-                    return "FUCK"
+                HTMLArray = StoreTemplate_File.readlines()
                 for line in range (len(HTMLArray)):
                     # print('[FlashCGG] Processing Line"', line, '" which is "', HTMLArray[line], '".')
-                    HTMLArray[line] = HTMLArray[line].replace("[NAME]", Name)
-                    HTMLArray[line] = HTMLArray[line].replace("[SHORT_DESC]", Short_Description)
-                    HTMLArray[line] = HTMLArray[line].replace("[LONG_DESC]", Long_Description)
-                    HTMLArray[line] = HTMLArray[line].replace("[VERSION]", Version)
-                    HTMLArray[line] = HTMLArray[line].replace("[LICENSE_YEAR]", License_Year)
-                    HTMLArray[line] = HTMLArray[line].replace("[LICENSE]", License)
-                    HTMLArray[line] = HTMLArray[line].replace("[GITHUB_PROFILE]", GitHub_Profile)
-                    HTMLArray[line] = HTMLArray[line].replace("[GITHUB_REPO]", GitHub_Repo)
-                    HTMLArray[line] = HTMLArray[line].replace("[GITHUB_CONTRIBUTORS]", GitHub_Contributors)
-                    HTMLArray[line] = HTMLArray[line].replace("[DISCORD_LINK]", Discord)
-                    HTMLArray[line] = HTMLArray[line].replace("[THEME]", SNDL_Theme)
-                    HTMLArray[line] = HTMLArray[line].replace("[EMBED_COLOR]", Embed_Color)
-                    HTMLArray[line] = HTMLArray[line].replace("[STORE_PAGE_NAME]", Store_Page_Name)
-                    HTMLArray[line] = HTMLArray[line].replace("[FOLDER_NAME]", Folder_Name)
-                    HTMLArray[line] = HTMLArray[line].replace("[EMBED_FILENAME]", Embed_FileName)
-                    HTMLArray[line] = HTMLArray[line].replace("[STORE_EMBED_FILENAME]", Store_Embed_FileName)
-                    HTMLArray[line] = HTMLArray[line].replace("[STORE_USER_FOLDER]", UserFolderName)
-                    HTMLArray[line] = HTMLArray[line].replace("[FLASHSTORE_API-EMBEDS]", MoreByCode)
+                    HTMLArray[line] = HTMLArray[line].replace("[SPLASH_TEXT]", SplashText_Selected)
+                    HTMLArray[line] = HTMLArray[line].replace("[FLASHCORD_OFFICIAL-MODULES]", ModuleCode)
+                    HTMLArray[line] = HTMLArray[line].replace("[REPLUGGED_PLUGINS]", PluginCode)
+                    HTMLArray[line] = HTMLArray[line].replace("[NON-FLASHCORD_THEMES]", ThemesCode)
+                    HTMLArray[line] = HTMLArray[line].replace("[API_VERSION]", API_Version)
+                    HTMLArray[line] = HTMLArray[line].replace("[API_LATENCY]", API_Latency)
+                    HTMLArray[line] = HTMLArray[line].replace("[LAST_UPDATE]", Last_Update_String)
+                    HTMLArray[line] = HTMLArray[line].replace("[COPYRIGHT_DATE]", Copyright_Date)
                     EditHTML_File.write(HTMLArray[line])
                     ProcessingProgress = ((line+1)/len(HTMLArray))*100
-                    print('[FlashCFG // HTML-CFG] Processed Line', line+1, '/', len(HTMLArray), '(', ProcessingProgress, '%).', StepFile)
-                    # print('[FlashCGG] Processed Line is now "', HTMLArray[line], '".')
-
-def FlashcordStoreConfig():
-    print("[FlashCFG] Script initiated.")
-    FileBackup("Store Page")
-    HTMLConfigurator(0)
-    FileBackup("Embed")
-    HTMLConfigurator(1)
-    print("[FlashCFG] Script complete.")
-    return
-
-FlashcordStoreConfig()
-"""
+                    print('[FlashCFG // HTML-CFG] Processed Line', line+1, '/', len(HTMLArray), '(', ProcessingProgress, '%).')
+        else:
+            with open(HTMLFile, 'r', encoding='utf-8') as EditHTML_File:
+                BuildData = EditHTML_File.readlines()
+            with open(HTMLFile, 'w', encoding='utf-8') as EditHTML_File:
+                BuildTime = math.floor(time.time() - StartTime)
+                BuildTime_String = str(BuildTime) + " seconds"
+                BuildData[70] = f'                        <p>Build time: {BuildTime_String}</p>\n'
+                EditHTML_File.writelines(BuildData)
+HTMLConfigurator(False)
+HTMLConfigurator(True)
